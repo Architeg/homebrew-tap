f2a38fa6fc9392af37980a6b6a6d2b0b072f4fde
architeg <valeriy.bagrintsev@gmail.com>
2026-07-24 00:24:42 +0300

    Update gloss to 0.1.1

--- Formula/gloss.rb
+++ Formula/gloss.rb
@@ -1,20 +1,20 @@
 class Gloss < Formula
   desc "Command glossary and alias helper"
   homepage "https://github.com/Architeg/gloss"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Architeg/gloss/releases/download/v0.1.0/gloss-darwin-arm64.zip"
-    sha256 "9ea5afb1267a940959bd0de288c26eb16b35e3ffa5336504080f0a8d7920fd35"
+    url "https://github.com/Architeg/gloss/releases/download/v0.1.1/gloss-darwin-arm64.zip"
+    sha256 "6224a8c1c421be199746269c6ab7b5843a02f0c8a50a42ddfa984e8f8ffafbd9"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Architeg/gloss/releases/download/v0.1.0/gloss-darwin-amd64.zip"
-    sha256 "d59b23bfcdc24639219262c27266054c96c65bc8e270106e975010d797fff730"
+    url "https://github.com/Architeg/gloss/releases/download/v0.1.1/gloss-darwin-amd64.zip"
+    sha256 "7afaea937078dc9361ea183471bc222bb46ea57a2ce0607fdf41d6594ab1ba6d"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Architeg/gloss/releases/download/v0.1.0/gloss-linux-amd64.zip"
-    sha256 "cd583184e306fb740538c73f7fab616bc4830cd1136bb55a296ca5dac7a30a45"
+    url "https://github.com/Architeg/gloss/releases/download/v0.1.1/gloss-linux-amd64.zip"
+    sha256 "37e8a005413f1aaaf66acee1fe2969b8cd7249e29cbe088593243bfdc1b9b096"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/Architeg/gloss/releases/download/v0.1.0/gloss-linux-arm64.zip"
-    sha256 "0ba8fb048fae6d8520c7ae5805b092db297eef98c11ad39a815eca7d18ef6ad9"
+    url "https://github.com/Architeg/gloss/releases/download/v0.1.1/gloss-linux-arm64.zip"
+    sha256 "1e72599168d59993ea7c764ea72e52aec9734dfa1759d5678fbcad8b7cd8ec81"
   end
 
   def install
@@ -22,6 +22,6 @@ class Gloss < Formula
   end
 
   test do
-    assert_match "gloss #{version}", shell_output("#{bin}/gloss version") 
+    assert_match "gloss #{version}", shell_output("#{bin}/gloss version")
   end
 end
